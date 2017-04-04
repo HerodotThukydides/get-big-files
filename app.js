7ad4c44899c1b44507e77ef230ab9076d2933027
HerodotThukydides <jptebbe@hotmail.com>
2017-04-04 19:35:49 +0200

    clean up

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 const LIST = require('./biggestFiles');
 
-///
+// No of elements on the output list
 var listLength = 25;
 
 // directory path to walk thorugh
   // put in your own directory (mind the escape charactert)
   // example with windows path
-var directory = "C:\\Users\\YOUR_USER_NAME\\Google Drive";
+var directory = "C:\\Users\\YOURNAME\\Google Drive";
 
-
-// TODO: solve callback hell
-// THIS HERE SHOULD WORK!
-// my function should return the list after
-// everything was solved
-// console.log( LIST.getFilesBySize(directory, listLength) );
-
-// by now just (the log is inside the function)
 LIST.getFilesBySize(directory, listLength);
